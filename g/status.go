9c00eeb6292a9fdb531c926657e3fffc70f1525a
dotwoo <dotwoo@baishancloud.com>
2016-11-14 12:37:08 +0800

    添加优雅重启支持，减少重启引起的数据丢失

---
+++ g/status.go
@@ -0,0 +1,41 @@
+package g
+
+import "sync"
+
+type ReceiverStatusManager struct {
+	sync.WaitGroup
+	lock   sync.RWMutex
+	isRun  bool
+	isStop bool
+}
+
+func NewReceiverStatusManager() *ReceiverStatusManager {
+	rsm := &ReceiverStatusManager{}
+	rsm.isRun = false
+	return rsm
+}
+
+func (r *ReceiverStatusManager) IsRun() bool {
+	r.lock.RLock()
+	defer r.lock.RUnlock()
+	return r.isRun
+}
+
+func (r *ReceiverStatusManager) IsStop() bool {
+	r.lock.RLock()
+	defer r.lock.RUnlock()
+	return r.isStop
+}
+
+func (r *ReceiverStatusManager) Run() {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	r.isRun = true
+
+}
+
+func (r *ReceiverStatusManager) Stop() {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	r.isRun = false
+}
